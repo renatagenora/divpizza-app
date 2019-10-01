a232d8baaef9978273418970c5002e1ed0da58c3
RENATA GENORA DUCWORTH DE ALMEIDA <507944616@alunos.etec.itaqua>
2019-09-30 22:22:07 -0300

    hoje colocamos range para colocar o quanto de pessoa comeriam a pizza

--- src/app/comprar-pizza/comprar-pizza.page.ts
+++ src/app/comprar-pizza/comprar-pizza.page.ts
@@ -8,14 +8,43 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ComprarPizzaPage implements OnInit {
   idpizza;
-  constructor(private activatedRoute:ActivatedRoute){
+  nomePizza;
+  descricaoPizza;
+  precoPizza;
+  divisor = 1;
+  precoPizzaFixo;
+  constructor(private activatedRoute: ActivatedRoute) {
     this.activatedRoute.snapshot.params.id
   }
 
   ngOnInit() {
   }
-  ionviewDidEnter(){
+  ionViewDidEnter() {
     this.idpizza = this.activatedRoute.snapshot.params.id
-  }
+    let pizzaString = localStorage.getItem(this.idpizza)
+    let pizzaObjeto = JSON.parse(pizzaString)
+    this.nomePizza = pizzaObjeto.nomePizza
+    this.descricaoPizza = pizzaObjeto.descricaoPizza
+    this.precoPizza = pizzaObjeto.precoPizza
+    this.precoPizzaFixo = this.precoPizza
 
+  }
+  incrementar() {
+    this.divisor++
+
+  }
+  decrementar() {
+    if (this.divisor > 1) {
+      this.divisor--
+      this.atualizarPreco()
+    }
+  }
+  atualizarPreco() {
+    this.precoPizza = (parseFloat(this.precoPizzaFixo) / this.divisor).toFixed(2)
+  }
+  atualizarcomRange(e) {
+    this.atualizarPreco()
+
+  }
 }
+
